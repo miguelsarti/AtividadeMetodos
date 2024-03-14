2c48dace634486740c8b100272743665ba7188c2
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-03-14 10:51:53 -0300

    Primeiro método

--- atividademetodos.js
+++ atividademetodos.js
@@ -1,1 +1,10 @@
 //10 métodos de Array em JavaScript
+
+//Método 1: .concat()
+//Utilizado para juntar dois ou mais arrays, e nos retorna um novo array
+
+const array = [1, 2, 3];
+const newArray = array.concat([4, 5]);
+
+console.log(array);
+console.log(newArray);
