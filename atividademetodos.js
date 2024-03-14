b12b6d3b186056e612bb931d9e29865e7dd45305
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-03-14 11:19:05 -0300

    Correção do primeiro método

--- atividademetodos.js
+++ atividademetodos.js
@@ -1,11 +1,24 @@
 //10 métodos de Array em JavaScript
 
-//Método 2: .join()
-//Utilizado para juntar todos os elementos de array
-//Em um string e retorna essa string
+//Método 1: .concat()
+//Utilizado para juntar dois ou mais arrays, e nos retorna um novo array
 
-const array = ["a", "b", "c"];
-const string = array.join("-");
+//EXEMPLO 1
+
+const array = [1, 2, 3];
+const newArray = array.concat([4, 5]);
+
+console.log("Exemplo 1");
 
 console.log(array);
-console.log(string);
+console.log(newArray);
+
+//EXEMPLO 2
+
+const array1 = [1, 3 , 5];
+const newArray1 = array1.concat([7,9]);
+
+console.log("Exemplo 2");
+
+console.log(array1);
+console.log(newArray1);
