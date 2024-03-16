b57f9e6fbef994e12f547c6295c17106d9fee5fb
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-03-16 20:23:48 -0300

    Quinto método

--- atividademetodos.js
+++ atividademetodos.js
@@ -1,22 +1,23 @@
 //10 métodos de Array em JavaScript
 
-//Método 4: .slice()
-//Utilizado para retornar uma cópia do array
-//para funcionar, é necessário colocar o número
-//da posição onde deve iniciar e onde deve terminar.
+//Método 5: .map()
+//Ultilizado para retornar uma nova array 
+//com a raiz quadrada de todos os valores selecionados
 
 console.log("Exemplo 1");
 
-const array = ["a", "b", "c", "d", "e"];
-const newArray = array.slice(1);
+const numbers = [4, 9, 16, 25];
+const newArray = numbers.map(Math.sqrt);
 
-console.log(array);
+console.log(numbers);
 console.log(newArray);
 
 console.log("Exemplo 2");
 
-const array1 = ["1", "2", "3", "4", "5"];
-const newArray1 = array1.slice(2, 5);
+const numbers1 = [36, 49, 64, 81];
+const newArray1 = numbers1.map(Math.sqrt);
 
-console.log(array1);
+console.log(numbers1);
 console.log(newArray1);
+
+
