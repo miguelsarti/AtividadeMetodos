4ab0f10a173f2fb65f175ad690384181a04b435a
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-03-16 21:01:21 -0300

    Sexto método

--- atividademetodos.js
+++ atividademetodos.js
@@ -1,23 +1,21 @@
 //10 métodos de Array em JavaScript
 
-//Método 5: .map()
-//Ultilizado para retornar uma nova array 
-//com a raiz quadrada de todos os valores selecionados
+//Método 6: .lenght
+//Utilizado para retornar os elementos de uma array 
+//de acordo com o número selecionado
 
+//Exemplo 1
 console.log("Exemplo 1");
 
-const numbers = [4, 9, 16, 25];
-const newArray = numbers.map(Math.sqrt);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.length = 2;
 
-console.log(numbers);
-console.log(newArray);
+console.log(fruits);
 
+//Exemplo 2
 console.log("Exemplo 2");
 
-const numbers1 = [36, 49, 64, 81];
-const newArray1 = numbers1.map(Math.sqrt);
+const fruits1 = ["5", "10", "15", "20", "25"];
+fruits1.length = 3;
 
-console.log(numbers1);
-console.log(newArray1);
-
-
+console.log(fruits1);
